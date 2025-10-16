75731e078d456687ea115749129aed6c45bd9965
harshavardhan-upputuri <harshaupputuri123@gmail.com>
2025-10-16 23:52:37 +0530

    Updated backend

--- backend/src/main/java/com/insurance/model/InsuranceOrder.java
+++ backend/src/main/java/com/insurance/model/InsuranceOrder.java
@@ -26,7 +26,7 @@
     public class InsuranceOrder {
         
         @Id
-        @GeneratedValue(strategy=GenerationType.AUTO)
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
         private Long id;
 
         private String orderId;
